3cdbdbc20da318c1fde0c104815ff85c5ace15f0
Andrey Khrolenok <andrey@khrolenok.ru>
2025-01-04 19:13:53 +0300

    Fix deprecation Data Entry Flow constants

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -4,8 +4,9 @@
 from unittest.mock import patch
 
 import pytest
-from homeassistant import config_entries, data_entry_flow
+from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResultType
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.beward.config_flow import BewardFlowHandler
@@ -39,7 +40,7 @@ async def test_config_flow_import(hass: HomeAssistant):
         data=config,
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["data"] == config
 
     with patch.object(
@@ -51,7 +52,7 @@ async def test_config_flow_import(hass: HomeAssistant):
             data=config,
         )
 
-        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+        assert result["type"] == FlowResultType.ABORT
 
 
 # Here we simulate a successful config flow from the backend.
@@ -65,7 +66,7 @@ async def test_successful_config_flow(hass: HomeAssistant, bypass_get_data):
     )
 
     # Check that the config flow shows the user form as the first step
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     # If a user were to enter `test_username` for username and `test_password`
@@ -76,7 +77,7 @@ async def test_successful_config_flow(hass: HomeAssistant, bypass_get_data):
 
     # Check that the config flow is complete and a new entry is created with
     # the input data
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == MOCK_HOST
     assert result["data"] == MOCK_CONFIG
     assert result["result"]
@@ -92,14 +93,14 @@ async def test_failed_config_flow(hass: HomeAssistant, error_on_get_data):
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"], user_input=MOCK_CONFIG
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["errors"] == {"base": "auth"}
 
     with patch.object(
@@ -111,7 +112,7 @@ async def test_failed_config_flow(hass: HomeAssistant, error_on_get_data):
             result["flow_id"], user_input=MOCK_CONFIG
         )
 
-        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+        assert result["type"] == FlowResultType.ABORT
 
 
 # Our config flow also has an options flow, so we must test it as well.
@@ -126,7 +127,7 @@ async def test_options_flow(hass: HomeAssistant):
     result = await hass.config_entries.options.async_init(entry.entry_id)
 
     # Verify that the first options step is a user form
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     # Enter some fake data into the form
@@ -136,7 +137,7 @@ async def test_options_flow(hass: HomeAssistant):
     )
 
     # Verify that the flow finishes
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == MOCK_HOST
 
     # Verify that the options were updated
@@ -159,5 +160,5 @@ async def test_failed_options_flow(hass: HomeAssistant):
     result = await hass.config_entries.options.async_init(entry.entry_id)
 
     # Verify that the first options step is a user form
-    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "no_options_available"
